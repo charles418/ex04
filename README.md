3367fd0c8565145350243f83585b881a7b99b558
charles418 <charlesgagnon84@gmail.com>
2013-09-28 19:03:40 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Charles Gagnon 
+
+Exercice 04
+
+Cours : SIO 2109 - Introduction à la programmation
+
+908 306 854
--- bin/ex04.dart
+++ bin/ex04.dart
@@ -1,20 +1,68 @@
-String fibonacciIteration(int t) {
-  int a = 0, b = 1, f = 1, n = 1;
-  String result = '0' ' 1';
-  while (n < t) {
-    f = a + b;
-    result = '$result $f';
-    a = b; 
-    b = f;
-    n = n + 1;
-  }
-  return result;
+import 'dart:math' as mathematique;
+void main (){
+  question1();
+  question2();
+  question3();
+  question4();
+  question5();
+  //ex04: Write functions with parametes and use them in the given context
 }
-main() {
-  int count = 10;
-  String sequence = 
-      fibonacciIteration(count);
-  print('Fibonacci with iteration: '
-        '${count + 1} numbers sequence');
-  print(sequence);
+
+void question1(){
+  //Print the first 11 numbers, starting with 0, of the Fibonacci sequence.
+    
+    print(fibonacciLine(11));}
+num fibonacci(num n) {
+    if (n == 0 || n == 1) {
+        return n;}
+    else
+{return fibonacci(n-1) + fibonacci(n-2);}}
+
+List fibonacciLine(num n) {
+    var list = new List(n);
+    for (var x = 0; x < n; x++) {
+        list[x] = fibonacci(x);
+    }
+    return list;}
+
+void question2() {
+  //Calculate the end point of the line
+  var xstart = 0;
+  var ystart = 0;
+  var height = 660;
+  var width = 980;
+  var angle = 33;
+  var linelenth = 120;
+  
+      
+ 
+}
+  
+  
+void question3() {  
+  //Calculate the time in years,months and days passed between your birthday at noon and today at noon
+  
+  DateTime bday = new DateTime(1987,3,19,12,0,0);
+  DateTime today = new DateTime(2013,9,30,12,0,0);
+  var difference = (today.difference(bday));
+  print ((difference.inDays));
+  
+}
+
+void question4() {
+  //Calculate a compound interest each year for 10 years of 100$ at a fixed interest rate of 3.3
+  
+  var interest = 0.033;
+  var invest = 1000;
+  var rate = (1+interest);
+  for (int o = 1; o < 11; o++)
+     print(invest*mathematique.pow(rate,o));
+}
+  
+void question5() {
+  //Calculate the first 20 terms of the 12 times table
+  
+  var y=12;
+  for (int x=1; x <=20; x++) 
+  print(x*y);
 }
