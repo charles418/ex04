e0f6a249c40018f40a76c977d189fdcac5f8533d
Dzenan Ridjanovic <dzenanr@gmail.com>
2013-10-03 13:04:01 -0400

    update dr.txt

--- bin/ex04.dart
+++ bin/ex04.dart
@@ -9,6 +9,39 @@ void main (){
   //ex04: Write functions with parametes and use them in the given context
 }
 
+/*
+Le code de Arnaud Genest:
+
+num fibonacci(num n) {
+    if (n == 0 || n == 1) {
+        return n;}
+    else
+{return fibonacci(n-1) + fibonacci(n-2);}}
+
+List fibonacciLine(num n) {
+    var list = new List(n);
+    for (var x = 0; x < n; x++) {
+        list[x] = fibonacci(x);
+    }
+    return list;}
+
+Le code de Charles Gagnon:
+
+num fibonacci(num n) {
+    if (n == 0 || n == 1) {
+        return n;}
+    else
+{return fibonacci(n-1) + fibonacci(n-2);}}
+
+List fibonacciLine(num n) {
+    var list = new List(n);
+    for (var x = 0; x < n; x++) {
+        list[x] = fibonacci(x);
+    }
+    return list;}
+
+ */
+
 void question1() {
   //Print the first 11 numbers, starting with 0, of the Fibonacci sequence.
   print(fibonacciLine(11));
@@ -38,17 +71,17 @@ void question2() {
   var angle = 33;
   var rad = angle * 0.0174532;
   var linelenth = 120;
-  var x = (linelenth * mathematique.cos(rad)) + locx; 
-  var y = (linelenth * mathematique.sin(rad)) + locy; 
+  var x = (linelenth * mathematique.cos(rad)) + locx;
+  var y = (linelenth * mathematique.sin(rad)) + locy;
   print("The coordinates of the end of the line is ($x , $y)");
 }
-  
-void question3() {  
+
+void question3() {
   //Calculate the time in years,months and days passed between your birthday at noon and today at noon
   DateTime bday = new DateTime(1987,3,19,12,0,0);
   DateTime today = new DateTime(2013,9,30,12,0,0);
   var difference = (today.difference(bday));
-  print ((difference.inDays));  
+  print ((difference.inDays));
 }
 
 void question4() {
@@ -59,10 +92,10 @@ void question4() {
   for (int o = 1; o < 11; o++)
     print(invest*mathematique.pow(rate,o));
 }
-  
+
 void question5() {
   //Calculate the first 20 terms of the 12 times table
   var y = 12;
-  for (int x = 1; x <= 20; x++) 
+  for (int x = 1; x <= 20; x++)
   print(x * y);
 }
